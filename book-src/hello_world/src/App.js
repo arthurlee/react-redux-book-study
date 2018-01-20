1b97d8d77c6ab52019382689f3c501659b494b27
arthurlee <boyal2008@gmail.com>
2018-01-20 23:12:24 +0800

    无状态函数版本

--- book-src/hello_world/src/App.js
+++ book-src/hello_world/src/App.js
@@ -1,8 +1,17 @@
 import React from 'react'
+
+// 无状态函数版本
+export function HelloApp() {
+	return <h1>Hello world from HelloApp!</h1>
+}
+
 export default class App extends React.Component {
 	render() {
 		return (
-			<h1>Hello world!</h1>
+			<div>
+				<h1>Hello world!</h1>
+				<HelloApp />
+			</div>
 		)
 	}
 }
